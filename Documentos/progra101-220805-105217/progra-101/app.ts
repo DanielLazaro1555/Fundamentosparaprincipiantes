f2b694f1b86affddaf88fb5964460bbd1d3163a9
danielhuaman <danielsam_m@hotmail.com>
2024-04-15 13:14:49 -0500

    Update app.ts

--- Documentos/progra101-220805-105217/progra-101/app.ts
+++ Documentos/progra101-220805-105217/progra-101/app.ts
@@ -1,25 +1,17 @@
-//console.log('Hola mundo!!');
-//Strings
-//a = nombre de la persona
-export let firstName = 'Daniel'; // Variable para almacenar el nombre
-let LastName = 'Huaman'; // Variable para almacenar el apellido
+//console.log('Inicio de programa');
+//console.log('Fin de programa');
 
-//console.log(firstName, LastName);
+//let isTired: boolean = !true;
 
-let fullName = firstName + " " + LastName; // Variable para almacenar el nombre completo
+//if (isTired) {
+//    console.log('Tomar café'); // Si está cansado, tomar café
+//} else {
+console.log('Tomar agua'); // Si no está cansado, tomar agua
+//}
 
-console.log(fullName); // Imprime el nombre completo
-
-let number1 = 10; // Variable para almacenar el primer número
-let number2 = 20; // Variable para almacenar el segundo número
-console.log(number1 + number2); // Imprime la suma de los dos números
-
-let isActive: boolean = true; // Variable booleana para indicar si está activo
-let isRunning: boolean = false; // Variable booleana para indicar si está corriendo
-let hashKids: boolean = true; // Variable booleana para indicar si tiene hijos
-
-isActive = !isActive; // Cambia el valor de isActive
-
-console.log("isActive: ", isActive); // Imprime el valor de isActive
-console.log("isRunning: ", isRunning); // Imprime el valor de isRunning
-console.log("hashKids: ", hashKids); // Imprime el valor de hashKids
+let grade: number = 90;
+if (grade >= 90) {
+    console.log('El alumno aprobó la clase!');
+} else {
+    console.log('El alumno NO aprueba la clase!');
+}
