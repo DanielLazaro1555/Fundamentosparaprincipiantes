72a1a0af368879abdfc07e0efe631ea7ca4dc4fb
danielhuaman <danielsam_m@hotmail.com>
2024-04-15 08:33:55 -0500

    Tipo de dato

--- Documentos/progra101-220805-105217/progra-101/app.ts
+++ Documentos/progra101-220805-105217/progra-101/app.ts
@@ -1,7 +1,15 @@
-console.log('Hola mundo!!');
+//console.log('Hola mundo!!');
 //Strings
 //a = nombre de la persona
 let firstName = 'Daniel';
 let LastName = 'Huaman';
 
-console.log(firstName + ' ' + LastName);
+//console.log(firstName, LastName);
+
+let fullName = firstName + " " + LastName;
+
+console.log(fullName);
+
+let number1 = 10;
+let number2 = 20;
+console.log(number1 + number2);
